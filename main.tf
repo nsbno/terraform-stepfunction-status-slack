e929939b2a44b757fef058eb2aab904b3d19e9b2
arnhaa <arnehaagensen@gmail.com>
2020-03-18 09:57:25 +0100

    revised data name

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-data "aws_caller_identity" "current-account" {}
+data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  current_account_id = data.aws_caller_identity.current-account.account_id
+  current_account_id = data.aws_caller_identity.current.account_id
   current_region     = data.aws_region.current.name
 }
 

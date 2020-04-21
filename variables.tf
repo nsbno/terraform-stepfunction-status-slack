3b86a958e8f0e1ecb1aabd72a73d90e8300d9922
stekern <stekern@users.noreply.github.com>
2020-04-21 12:14:13 +0200

    Make artifact_bucket_name variable optional

--- variables.tf
+++ variables.tf
@@ -3,20 +3,23 @@ variable "name_prefix" {
   type        = string
 }
 
+# This argument is not being used and should probably be removed in future updates
+# Currently using a default value for it to ensure backwards-compatibility.
 variable "artifact_bucket_name" {
   description = "The name of the bucket used for trigger files and artifacts"
-  type = string
+  type        = string
+  default     = ""
 }
 
 variable "slackwebhook" {
   description = "The slack webhook URL"
-  type = string
+  type        = string
 }
 
 variable "statestonotify" {
   description = "Sets the lambda to forward events based on values: all or errors"
-  type = string
-  default = "all"
+  type        = string
+  default     = "all"
 }
 
 variable "tags" {

1c62a2ce5ac5b2a3f75bc7df8865966fabb89c58
stekern <stekern@users.noreply.github.com>
2020-12-18 14:04:23 +0100

    Fix CloudWatch event format

--- main.tf
+++ main.tf
@@ -51,7 +51,10 @@ resource "aws_cloudwatch_event_rule" "deploy_events_rule" {
     "Step Functions Execution Status Change"
   ],
   "detail": {
-    "stateMachineArn": ${jsonencode(var.state_machine_arns)}
+    "stateMachineArn": ${
+  length(var.state_machine_arns) > 0
+  ? jsonencode(var.state_machine_arns)
+: jsonencode([{ exists = true }])}
   }
 }
 EOF
